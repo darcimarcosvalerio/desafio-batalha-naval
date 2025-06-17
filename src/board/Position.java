7db0c87d955451dbcb44fce3ca7a9a171d9b8950
Darcimarcos <darcimarcos@hotmail.com>
2025-06-17 00:26:54 -0300

    Position.java
    2. Pacote board

---
+++ src/board/Position.java
@@ -0,0 +1,46 @@
+// board/Position.java
+package board;
+
+public class Position {
+    private int row;
+    private int column;
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + row + ", " + column + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return row == position.row && column == position.column;
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(row, column);
+    }
+}
